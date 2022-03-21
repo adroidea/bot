9b635a3e110a80c10152061ece961e537dfe600a
adan-ea <57812567+adan-ea@users.noreply.github.com>
2022-03-21 16:31:33 +0100

    :construction: (TwitchAlerts) : went for async

--- events/twitch/twitchAlert.js
+++ events/twitch/twitchAlert.js
@@ -7,29 +7,25 @@ const twitch = new TwitchApi({
 });
 
 async function getStream() {
-    const streams = await twitch.getStreams({channel: 'igua30'});
-    console.log(streams.data[0].thumbnail_url);
+    const streams = await twitch.getStreams({channel: 'tatalyssia'});
     return streams.data[0];
 }
 
 module.exports = {
     name: 'messageCreate',
     once: false,
-    execute(client, message) {
-
+    async execute(client, message) {
         if (message.content === 'test') {
-            //`${r.user_name} is live on ${r.game_name} for ${r.viewer_count} viewers`
-            getStream().then(r => {
-                const embed = new MessageEmbed()
-                    .setTitle(`${r.title}`)
-                    .setURL(`https://twitch.tv/${r.user_name}`)
-                    .setThumbnail(r.thumbnail_url)
-                    .setDescription(`adan_ea is live !`)
-                    .addField(`**Game**`, r.game_name, false)
-                    .setColor('#b02020');
-                message.channel.send(`<@&930051152987430952> ${r.user_name} est en live ! Rejoignez le sur ${r.game_name}`, {embeds: [embed]});
-            });
-
+            const r = await getStream();
+            const embed = new MessageEmbed()
+                .setTitle(`${r.title}`)
+                .setURL(`https://twitch.tv/${r.user_name}`)
+                .setDescription(`**${r.user_name} is live for ${r.viewer_count} viewers !**`)
+                .addField(`**Game**`, r.game_name, false)
+                .setImage(r.getThumbnailUrl())
+                .setColor('#b02020');
+            //TODO: find how to send a message and an embed together
+            await message.channel.send(`<@&930051152987430952> ${r.user_name} is live ! Join them for some ${r.game_name}`, {embeds: [embed]});
         }
     }
 };
--- utils/handlers/EventUtil.js
+++ utils/handlers/EventUtil.js
@@ -3,22 +3,28 @@ const {glob} = require('glob');
 
 const pGlob = promisify(glob);
 
+let nbEvents = 0;
+let nbFailedEvents = 0;
+
 module.exports = async client => {
     (await pGlob(`${process.cwd()}/events/*/*.js`)).map(async eventFile => {
         const event = require(eventFile);
 
         if(!eventList.includes(event.name) || !event.name) {
-            return console.log(`-----\nNot initialised Event: Double check the name\n File : ${eventFile}\n-----`)
+            nbFailedEvents++;
+            return console.log(`-----\nNot initialised Event: Double check the name\n File : ${eventFile}\n-----`);
         }
-
+        
         if(event.once) {
             client.once(event.name, (...args) => event.execute(client, ...args));
         } else {
             client.on(event.name, (...args) => event.execute(client, ...args));
         }
-
-        console.log(`Loaded Event : ${event.name}`);
+        
+        nbEvents++
     })
+    if(nbEvents !== 0) await console.log(`${nbEvents} events loaded. `);
+    if(nbFailedEvents !== 0) await console.log(`Failed to load ${nbFailedEvents} events. `);
 };
 
 const eventList = ['apiRequest', 'apiResponse', 'applicationCommandCreate', 'applicationCommandDelete', 'applicationCommandUpdate', 'channelCreate', 'channelDelete', 'channelPinsUpdate', 'channelUpdate', 'debug', 'emojiCreate', 'emojiDelete', 'emojiUpdate', 'error', 'guildBanAdd', 'guildBanRemove', 'guildCreate', 'guildDelete', 'guildIntegrationsUpdate', 'guildMemberAdd', 'guildMemberAvailable', 'guildMemberRemove', 'guildMembersChunk', 'guildMemberUpdate', 'guildScheduledEventCreate', 'guildScheduledEventDelete', 'guildScheduledEventUpdate', 'guildScheduledEventUserAdd', 'guildScheduledEventUserRemove', 'guildUnavailable', 'guildUpdate', 'interaction', 'interactionCreate', 'invalidated', 'invalidRequestWarning', 'inviteCreate', 'inviteDelete', 'message', 'messageCreate', 'messageDelete', 'messageDeleteBulk', 'messageReactionAdd', 'messageReactionRemove', 'messageReactionRemoveAll', 'messageReactionRemoveEmoji', 'messageUpdate', 'presenceUpdate', 'rateLimit', 'ready', 'roleCreate', 'roleDelete', 'roleUpdate', 'shardDisconnect', 'shardError', 'shardReady', 'shardReconnecting', 'shardResume', 'stageInstanceCreate', 'stageInstanceDelete', 'stageInstanceUpdate', 'stickerCreate', 'stickerDelete', 'stickerUpdate', 'threadCreate', 'threadDelete', 'threadListSync', 'threadMembersUpdate', 'threadMemberUpdate', 'threadUpdate', 'typingStart', 'userUpdate', 'voiceStateUpdate', 'warn', 'webhookUpdate'];
