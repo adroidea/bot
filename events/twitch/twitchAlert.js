a94004b9eb8e2365cbf4a86dc265b1871997790d
adan-ea <57812567+adan-ea@users.noreply.github.com>
2022-03-23 16:24:20 +0100

    feat (twitchAlert) : at stream start, send message and change discord pp

--- events/twitch/twitchAlert.js
+++ events/twitch/twitchAlert.js
@@ -6,26 +6,57 @@ const twitch = new TwitchApi({
     client_secret: process.env.TWITCH_CLIENT_SECRET
 });
 
-async function getStream() {
-    const streams = await twitch.getStreams({channel: 'tatalyssia'});
-    return streams.data[0];
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+let IsLiveMemory = false;
+
+async function run(client) {
+    await twitch.getStreams({channel: 'adroid_ea'}).then(async data => {
+        const r = data.data[0];
+
+        if (r !== undefined) {
+            if (r.type === 'live') {
+                if (IsLiveMemory === false || IsLiveMemory === undefined) {
+                    const embed = new MessageEmbed()
+                        .setTitle(`${r.title}`)
+                        .setURL(`https://twitch.tv/${r.user_name}`)
+                        .setDescription(`**${r.user_name} is live for ${r.viewer_count} viewers !**`)
+                        .addField(`**Game**`, r.game_name, false)
+                        .setImage(r.getThumbnailUrl())
+                        .setColor('#b02020');
+                    const sentMessage = client.channels.cache.get('949252153225150524').send({
+                        content: `<@&930051152987430952> ${r.user_name} is live ! Join them for some ${r.game_name}`,
+                        embeds: [embed]
+                    });
+                    await client.guilds.cache.get('605053128148254724').setIcon('https://cdn.discordapp.com/attachments/771934231647223848/946326723593633792/Peace_was_never_an_option.png');
+                    IsLiveMemory = true;
+                }
+            } else {
+                if (IsLiveMemory === true) {
+                    await client.guilds.cache.get('605053128148254724').setIcon('https://cdn.discordapp.com/attachments/771934231647223848/938389858802606160/jpp_jean-pierre.png');
+                    IsLiveMemory = false;
+                    sleep(900000);
+                }
+            }
+        } else {
+            if (IsLiveMemory === true) {
+                await client.guilds.cache.get('605053128148254724').setIcon('https://cdn.discordapp.com/attachments/771934231647223848/938389858802606160/jpp_jean-pierre.png');
+                IsLiveMemory = false;
+                sleep(900000);
+            }
+        }
+    });
+};
+
 module.exports = {
-    name: 'messageCreate',
-    once: false,
-    async execute(client, message) {
-        if (message.content === 'test') {
-            const r = await getStream();
-            const embed = new MessageEmbed()
-                .setTitle(`${r.title}`)
-                .setURL(`https://twitch.tv/${r.user_name}`)
-                .setDescription(`**${r.user_name} is live for ${r.viewer_count} viewers !**`)
-                .addField(`**Game**`, r.game_name, false)
-                .setImage(r.getThumbnailUrl())
-                .setColor('#b02020');
-            //TODO: find how to send a message and an embed together
-            await message.channel.send(`<@&930051152987430952> ${r.user_name} is live ! Join them for some ${r.game_name}`, {embeds: [embed]});
+    name: 'ready',
+    once: true,
+    async execute(client) {
+        while (true) {
+            await run(client);
+            await sleep(15000);
         }
     }
 };
