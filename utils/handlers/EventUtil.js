c072e1451b81632fa31cedaed6497baf25b2eb2d
adan-ea <57812567+adan-ea@users.noreply.github.com>
2022-03-23 16:24:20 +0100

    feat (twitchAlert) : at stream start, send message and change discord pp

--- utils/handlers/EventUtil.js
+++ utils/handlers/EventUtil.js
@@ -10,21 +10,34 @@ module.exports = async client => {
     (await pGlob(`${process.cwd()}/events/*/*.js`)).map(async eventFile => {
         const event = require(eventFile);
 
-        if(!eventList.includes(event.name) || !event.name) {
+        if (!eventList.includes(event.name) || !event.name) {
             nbFailedEvents++;
             return console.log(`-----\nNot initialised Event: Double check the name\n File : ${eventFile}\n-----`);
         }
-        
-        if(event.once) {
+
+        if (event.once) {
             client.once(event.name, (...args) => event.execute(client, ...args));
         } else {
             client.on(event.name, (...args) => event.execute(client, ...args));
         }
-        
-        nbEvents++
-    })
-    if(nbEvents !== 0) await console.log(`${nbEvents} events loaded. `);
-    if(nbFailedEvents !== 0) await console.log(`Failed to load ${nbFailedEvents} events. `);
+
+        nbEvents++;
+    });
+    if (nbEvents !== 0) await console.log(`${nbEvents} events loaded. `);
+    if (nbFailedEvents !== 0) await console.log(`Failed to load ${nbFailedEvents} events. `);
 };
 
-const eventList = ['apiRequest', 'apiResponse', 'applicationCommandCreate', 'applicationCommandDelete', 'applicationCommandUpdate', 'channelCreate', 'channelDelete', 'channelPinsUpdate', 'channelUpdate', 'debug', 'emojiCreate', 'emojiDelete', 'emojiUpdate', 'error', 'guildBanAdd', 'guildBanRemove', 'guildCreate', 'guildDelete', 'guildIntegrationsUpdate', 'guildMemberAdd', 'guildMemberAvailable', 'guildMemberRemove', 'guildMembersChunk', 'guildMemberUpdate', 'guildScheduledEventCreate', 'guildScheduledEventDelete', 'guildScheduledEventUpdate', 'guildScheduledEventUserAdd', 'guildScheduledEventUserRemove', 'guildUnavailable', 'guildUpdate', 'interaction', 'interactionCreate', 'invalidated', 'invalidRequestWarning', 'inviteCreate', 'inviteDelete', 'message', 'messageCreate', 'messageDelete', 'messageDeleteBulk', 'messageReactionAdd', 'messageReactionRemove', 'messageReactionRemoveAll', 'messageReactionRemoveEmoji', 'messageUpdate', 'presenceUpdate', 'rateLimit', 'ready', 'roleCreate', 'roleDelete', 'roleUpdate', 'shardDisconnect', 'shardError', 'shardReady', 'shardReconnecting', 'shardResume', 'stageInstanceCreate', 'stageInstanceDelete', 'stageInstanceUpdate', 'stickerCreate', 'stickerDelete', 'stickerUpdate', 'threadCreate', 'threadDelete', 'threadListSync', 'threadMembersUpdate', 'threadMemberUpdate', 'threadUpdate', 'typingStart', 'userUpdate', 'voiceStateUpdate', 'warn', 'webhookUpdate'];
+const eventList = ['apiRequest', 'apiResponse', 'applicationCommandCreate', 'applicationCommandDelete',
+    'applicationCommandUpdate', 'channelCreate', 'channelDelete', 'channelPinsUpdate', 'channelUpdate',
+    'debug', 'emojiCreate', 'emojiDelete', 'emojiUpdate', 'error', 'guildBanAdd', 'guildBanRemove',
+    'guildCreate', 'guildDelete', 'guildIntegrationsUpdate', 'guildMemberAdd', 'guildMemberAvailable',
+    'guildMemberRemove', 'guildMembersChunk', 'guildMemberUpdate', 'guildScheduledEventCreate',
+    'guildScheduledEventDelete', 'guildScheduledEventUpdate', 'guildScheduledEventUserAdd',
+    'guildScheduledEventUserRemove', 'guildUnavailable', 'guildUpdate', 'interaction', 'interactionCreate',
+    'invalidated', 'invalidRequestWarning', 'inviteCreate', 'inviteDelete', 'message', 'messageCreate',
+    'messageDelete', 'messageDeleteBulk', 'messageReactionAdd', 'messageReactionRemove', 'messageReactionRemoveAll',
+    'messageReactionRemoveEmoji', 'messageUpdate', 'presenceUpdate', 'rateLimit', 'ready', 'roleCreate',
+    'roleDelete', 'roleUpdate', 'shardDisconnect', 'shardError', 'shardReady', 'shardReconnecting',
+    'shardResume', 'stageInstanceCreate', 'stageInstanceDelete', 'stageInstanceUpdate', 'stickerCreate',
+    'stickerDelete', 'stickerUpdate', 'threadCreate', 'threadDelete', 'threadListSync', 'threadMembersUpdate',
+    'threadMemberUpdate', 'threadUpdate', 'typingStart', 'userUpdate', 'voiceStateUpdate', 'warn', 'webhookUpdate'];
