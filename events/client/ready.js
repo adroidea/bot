0ab8dbfb0729a37d0aab40f905a7dac2b35272fb
adan-ea <57812567+adan-ea@users.noreply.github.com>
2022-03-29 01:07:56 +0200

    refactor (Project) : Ready for release

--- events/client/ready.js
+++ events/client/ready.js
@@ -3,7 +3,7 @@ module.exports = {
     once: true,
     async execute(client) {
         console.log(`Bot ${client.user.tag} up and running !`);
-        const devGuild = await client.guilds.cache.get('605053128148254724');
+        const devGuild = await client.guilds.cache.get('814621177770541076');
         await devGuild.commands.set(client.commands.map(cmd => cmd));
     }
 };
