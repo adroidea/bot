4090d843dacad7de68e326e91b21ef32007095ef
--global <--global>
2023-01-08 13:10:48 +0100

    feat: More random messages conserning the live

---
+++ events/twitch/twitchSentences.js
@@ -0,0 +1,41 @@
+export const liveStart = [
+    "vient tout juste de lancer un stream ! Viens pour voir du",
+    "stream actuellement, il manque plus que toi. Rejoins nous pour du",
+    "a enfin lancé son stream ! Go prendre tes snacks et regarder du",
+    "est enfin en live ! J'espère que t'as de quoi manger pour regarder du",
+    "vient de lancer son stream, alors ramène ton petit boule qui chamboule pour du",
+    "est en live ! Rejoins le pour du",
+    "va te montrer ses skills (ou pas) en stream ! Viens vite pour ne pas le rater sur",
+    "est pas là! Mais il est où ? Bah sur",
+    "vient d'arriver ksksks, et si toi aussi tu arrivais ? On va tous s'amuser sur",
+    "a une absence incroyable de skill à te présenter sur",
+    "a ou n'a pas un burger chèvre miel, à toi de le découvrir en venant, tu pourras profiter pour regarder du non skill sur",
+    "Viens. C'est pas une demande, c'est un ordre"
+];
+
+export const gameChangePartOne = [
+    "Bon, on a eu marre de faire du",
+    "Votre streamer vient tout juste de rage quit",
+    "On veut plus faire de", 
+    "On a été fatigué par", 
+    "Le petit alt f4 des familles a été fait sur",
+    "On suit le planning (ca m'étonnerai que ce soit vrai) donc on quitte"
+];
+
+export const gameChangePartTwo = [
+    "Du coup on passe sur",
+    "On va donc faire un petit tour sur"
+];
+
+export const gameChangePartThree = [
+    "(Adan est pas fou en vrai sur ça, mais on l'aime bien quand même)",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    ""
+];  
