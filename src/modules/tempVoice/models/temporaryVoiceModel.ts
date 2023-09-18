c18024674754989cba0e2d343512270b0f933449
adan_ea <57812567+adan-ea@users.noreply.github.com>
2023-09-18 17:33:26 +0200

    feat(qotd): Warn on low questions left. Based on a user chosen threshold

--- src/modules/tempVoice/models/temporaryVoiceModel.ts
+++ src/modules/tempVoice/models/temporaryVoiceModel.ts
@@ -2,12 +2,12 @@ import mongoose from 'mongoose';
 
 export interface ITemporaryVoice {
     enabled: boolean;
-    hostChannels?: string[];
-    protectedChannels?: string[];
+    hostChannels: string[];
+    protectedChannels: string[];
 }
 
 export const temporaryVoiceSchema = new mongoose.Schema<ITemporaryVoice>({
-    enabled: { type: Boolean, default: false },
-    hostChannels: { type: [String], default: [] },
-    protectedChannels: { type: [String], default: [] }
+    enabled: { type: Boolean, default: false, required: true },
+    hostChannels: { type: [String], default: [], required: true },
+    protectedChannels: { type: [String], default: [], required: true }
 });
